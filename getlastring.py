8190fe057d29d7791edfd7a6424ca60e3d5f2040
Dan Taeyoung <provolot@gmail.com>
2017-07-18 17:23:59 -0400

    gitignore and path stuff

--- getlastring.py
+++ getlastring.py
@@ -7,7 +7,7 @@ from pprint import pprint
 with open('Secrets.json') as data_file:    
     Secrets = json.load(data_file)
 
-    db = TinyDB('db.json')
+    db = TinyDB('/home/provolot/_CRONTABBED/messyring/db.json')
     Bell = Query()
 
     slack = slackweb.Slack(url=Secrets[u'slack'][u'webhookUri'])
@@ -22,5 +22,7 @@ with open('Secrets.json') as data_file:
         print("New Doorbell!: " + str(doorbell.last_recording_id))
         url = doorbell.recording_url(doorbell.last_recording_id)
         db.insert({'doorbell_recording_id':doorbell.last_recording_id})
-        #slack.notify(channel="#_front_door", username="Doorbell-bot", icon_emoji=":robot_face:", text="Video from the most recent ring: <" + url + "|click here>");
+        slack.notify(channel="#_front_door", username="Doorbell-bot", icon_emoji=":robot_face:", text="Video from the most recent ring: <" + url + "|click here>");
+    else:
+        print("No new doorbell.")
 
